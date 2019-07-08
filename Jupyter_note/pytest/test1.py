6483d9b2a1ae7ad19b771656cc8d8b1225e1df9b
mtokons <mhasnainn@gmail.com>
2019-07-08 14:33:31 +0200

    update 08.07.19v1

--- Jupyter_note/pytest/test1.py
+++ Jupyter_note/pytest/test1.py
@@ -3,13 +3,13 @@
 from owlready2 import *
 import types
 
-onto = get_ontology("matcal.owl")
+onto = get_ontology("ai201.owl").load()
 
 
 with onto:
     class IngredientTest (Thing):
         pass
 
-sync_reasoner()
+#sync_reasoner()
 onto.save(file = 'matcal.owl', format = 'rdfxml')
 
