7e41562318fa8851225ac4402de59214ab9ff12e
mtokons <mhasnainn@gmail.com>
2019-07-12 21:00:59 +0200

    12.07 update

--- Jupyter_note/pytest/test1.py
+++ Jupyter_note/pytest/test1.py
@@ -5,10 +5,13 @@ import types
 
 onto = get_ontology("ai201.owl").load()
 
+x = ['abs', 'acs', 'ads']
 
-with onto:
-    class IngredientTest (Thing):
-        pass
+for yb in x:
+    with onto:
+        NewClass = types.new_class(yb, (A,))
+    # class yb (Thing):
+    #     namespace = onto
 
 #sync_reasoner()
 onto.save(file = 'matcal.owl', format = 'rdfxml')
