8207d3060cfa85d35646a9e958f3360ca5ff1416
mtokons <mhasnainn@gmail.com>
2019-08-19 00:39:46 +0200

    owl custom

--- Jupyter_note/pytest/test1.py
+++ Jupyter_note/pytest/test1.py
@@ -5,11 +5,15 @@ import types
 
 onto = get_ontology("ai201.owl").load()
 
+with onto:
+    class A (Thing):
+        pass
+
 x = ['abs', 'acs', 'ads']
 
 for yb in x:
     with onto:
-        NewClass = types.new_class(yb, (A,))
+        NewClass = types.new_class(yb, (Thing,))
     # class yb (Thing):
     #     namespace = onto
 
